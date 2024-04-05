1e2e0e4943c3368ec9b89035089b4a50030ca76d
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-05 12:52:12 -0700

    Added Implementation of ceating a login profile for User in IAM

---
+++ src/main/java/org/example/CreateLoginProfile.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.CreateLoginProfileRequest;
+import software.amazon.awssdk.services.iam.model.CreateLoginProfileResponse;
+import software.amazon.awssdk.services.iam.model.IamException;
+
+public class CreateLoginProfile {
+    public static void main(String[] args) {
+        try{
+            IamClient iam = IamClient.builder().build();
+            String username = "JavaUpdated";
+            String password = "Password123@";
+            CreateLoginProfileRequest request = CreateLoginProfileRequest.builder()
+                    .userName(username)
+                    .password(password)
+                    .passwordResetRequired(false)
+                    .build();
+            CreateLoginProfileResponse response = iam.createLoginProfile(request);
+            System.out.println("Login profile created for user :"+ username);
+            iam.close();
+        }catch(IamException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+    }
+}
