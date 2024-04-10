fec645965cb3f10cc81941c48abb9b2ef9236cc3
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-10 14:49:31 -0700

    Added code to free up IAM resource post group creation

--- src/main/java/org/example/CreateIAMGroup.java
+++ src/main/java/org/example/CreateIAMGroup.java
@@ -9,12 +9,13 @@ public class CreateIAMGroup {
     public static void main(String[] args) {
         try{
             IamClient iam = IamClient.builder().build();
-            String groupname = "MyNewGroup";
+            String groupname = "MyNewGroup"; //create your group here
             CreateGroupRequest request = CreateGroupRequest.builder()
                     .groupName(groupname)
                     .build();
             CreateGroupResponse response = iam.createGroup(request);
             System.out.println("Group created : "+ groupname + " "+ response.group().arn());
+            iam.close(); //never forget to free your IAM resource
         }catch (IamException e){
             System.err.println(e.awsErrorDetails().errorMessage());
         }
