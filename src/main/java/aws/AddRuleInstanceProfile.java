baebf348c606196918f2a38c8004adf8e5f87e99
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-13 16:57:51 -0700

    Implement attaching ready-role to an IAM instance

--- src/main/java/aws/AddRuleInstanceProfile.java
+++ src/main/java/aws/AddRuleInstanceProfile.java
@@ -1,2 +1,29 @@
-package aws;public class AddRuleInstanceProfile {
+package aws;
+
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.AddRoleToInstanceProfileRequest;
+import software.amazon.awssdk.services.iam.model.AddRoleToInstanceProfileResponse;
+import software.amazon.awssdk.services.iam.model.IamException;
+
+public class AddRuleInstanceProfile {
+    public static void main(String[] args) {
+
+        try {
+            IamClient iam = IamClient.builder().build();
+            String rolename = "MyNewJavaRole";
+            String instanceProfileName = "MyNewJavaProfile";
+
+            AddRoleToInstanceProfileRequest request = AddRoleToInstanceProfileRequest.builder()
+                    .instanceProfileName(instanceProfileName)
+                    .roleName(rolename)
+                    .build();
+
+            AddRoleToInstanceProfileResponse response = iam.addRoleToInstanceProfile(request);
+            System.out.println("Added role :"+rolename + " to instance profile: "+ instanceProfileName);
+
+        }catch(IamException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+    }
 }
+
