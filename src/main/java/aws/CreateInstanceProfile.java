fe7b076fe7e929721b31a0f0dd080da0492c82a0
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-13 16:07:15 -0700

    Added Implementaiton of how to create an Instance Profile

---
+++ src/main/java/aws/CreateInstanceProfile.java
@@ -0,0 +1,28 @@
+package aws;
+
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.CreateInstanceProfileRequest;
+import software.amazon.awssdk.services.iam.model.CreateInstanceProfileResponse;
+import software.amazon.awssdk.services.iam.model.IamException;
+import software.amazon.awssdk.services.iam.model.InstanceProfile;
+
+public class CreateInstanceProfile {
+    public static void main(String[] args) {
+        try{
+            IamClient iam = IamClient.builder().build();
+            String instanceProfileName = "MyNewJavaProfile";
+
+            CreateInstanceProfileRequest request = CreateInstanceProfileRequest.builder()
+                    .instanceProfileName(instanceProfileName)
+                    .build();
+
+            CreateInstanceProfileResponse response = iam.createInstanceProfile(request);
+            InstanceProfile createdInstanceProfile = response.instanceProfile();
+            System.out.println("IAM instance profile created: "+ createdInstanceProfile.instanceProfileName());
+            iam.close();
+        }catch(IamException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+
+        }
+    }
+}
