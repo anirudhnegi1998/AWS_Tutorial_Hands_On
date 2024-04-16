f015724200180f6dd6f130389019618538878106
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-16 15:19:12 -0700

    Implemented logic to remove tags from specific user by providing the tag keys as a list

---
+++ src/main/java/aws/RemoveTags.java
@@ -0,0 +1,32 @@
+package aws;
+
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.IamException;
+import software.amazon.awssdk.services.iam.model.UntagUserRequest;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class RemoveTags {
+    public static void main(String[] args) {
+        IamClient iam = IamClient.builder().build();
+        String username = "NewUser";
+        List<String> tagkeys = new ArrayList<>();
+
+        tagkeys.add("Department");
+        tagkeys.add("Project");
+
+        UntagUserRequest untagUserRequest = UntagUserRequest.builder()
+                .userName(username)
+                .tagKeys(tagkeys)
+                .build();
+
+        try{
+            iam.untagUser(untagUserRequest);
+            System.out.println("Untagged user successfully: "+ username);
+        }catch(IamException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+
+    }
+}
