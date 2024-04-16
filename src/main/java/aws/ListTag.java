718ceb36900b4bcf1591ec19df990f69711db3ed
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-16 15:13:31 -0700

    Implemented the logic to retrieve tags from all the users and list them along with the tag's key and value

--- src/main/java/aws/ListTag.java
+++ src/main/java/aws/ListTag.java
@@ -1,2 +1,37 @@
-package aws;public class ListTag {
+package aws;
+
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.*;
+
+import java.util.List;
+
+public class ListTag {
+    public static void main(String[] args) {
+        IamClient iam = IamClient.builder().build();
+        try{
+            ListUsersRequest listUsersRequest = ListUsersRequest.builder().build();
+            ListUsersResponse listUsersResponse = iam.listUsers(listUsersRequest);
+
+            for(User user : listUsersResponse.users()) {
+                //System.out.println(user); print it in order to check all the details of user like USerID, Arn
+                String userName = user.userName();
+                ListUserTagsRequest listUserTagsRequest = ListUserTagsRequest.builder().userName(userName).build();
+                ListUserTagsResponse listUserTagsResponse = iam.listUserTags(listUserTagsRequest);
+
+                List<Tag> tags = listUserTagsResponse.tags();
+                System.out.println("User : " + userName+ " Tags : " + tags.size());
+
+                if(tags.size() > 0){
+                    for(Tag tag : tags){
+                        System.out.println(tag.key() + " : " + tag.value());
+                    }
+                    System.out.println();
+                } else {
+                    System.out.println("No tags found");
+                }
+            }
+        }catch (IamException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+    }
 }
