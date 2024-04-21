a586549575c7da25e3728b0f4b1faa04ffe967b5
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-21 09:44:03 -0700

    Implemented creation of IAM policy through JAVA SDK

--- src/main/java/org/example_IAM/CreateIAMPolicy.java
+++ src/main/java/org/example_IAM/CreateIAMPolicy.java
@@ -1,15 +1,35 @@
-public class CreateIAMPolicy {
-    static void doIt(int x, int y, int m) {
-        if (x == 5) {
-            m=y;
-        } else {
-            m=x;
-        }
-    }
+package org.example_IAM;
 
+import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.CreatePolicyRequest;
+import software.amazon.awssdk.services.iam.model.CreatePolicyResponse;
+import software.amazon.awssdk.services.iam.model.IamException;
+
+public class CreateIAMPolicy {
     public static void main(String[] args) {
-        int i=6, j=4, k=9;
-        CreateIAMPolicy.doIt(i, j, k);
-        System.out.print(k);
+        try{
+            IamClient iam = IamClient.builder().build();
+            String policyDocument = "{\n" +
+                    "  \"Version\": \"2012-10-17\",\n" +
+                    "  \"Statement\": [\n" +
+                    "    {\n" +
+                    "      \"Effect\": \"Allow\",\n" +
+                    "      \"Action\": \"logs:CreateLogGroup\",\n" +
+                    "      \"Resource\": \"arn:aws:logs:us-west-2:123456789012:*\"\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}";
+
+            CreatePolicyRequest request = CreatePolicyRequest.builder()
+                    .policyName("NewPolicy")
+                    .policyDocument(policyDocument)
+                    .build();
+
+            CreatePolicyResponse response = iam.createPolicy(request);
+            System.out.println("Policy created: " + response.policy());
+            iam.close();
+        }catch(IamException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
     }
 }
