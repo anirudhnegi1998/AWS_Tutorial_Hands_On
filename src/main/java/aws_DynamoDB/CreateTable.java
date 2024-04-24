ab37824c2ddc0831295ff75fd8e5824db63ac5e7
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-23 22:42:48 -0700

    Implemented creation of table in DynamoDB

---
+++ src/main/java/aws_DynamoDB/CreateTable.java
@@ -0,0 +1,41 @@
+package aws_DynamoDB;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.*;
+
+public class CreateTable {
+    public static void main(String[] args) {
+        DynamoDbClient dynamoDbClient = DynamoDbClient.builder().build();
+        try{
+            AttributeDefinition partitionKey = AttributeDefinition.builder()
+                    .attributeName("UserID")
+                    .attributeType(ScalarAttributeType.S)
+                    .build();
+
+            KeySchemaElement keySchemaElement = KeySchemaElement.builder()
+                    .attributeName("UserID")
+                    .keyType(KeyType.HASH)
+                    .build();
+
+            ProvisionedThroughput provisionedThroughput = ProvisionedThroughput.builder()
+                    .readCapacityUnits(5L)
+                    .writeCapacityUnits(5L)
+                    .build();
+
+            CreateTableRequest createTableRequest = CreateTableRequest.builder()
+                    .tableName("UserTable")q
+                    .attributeDefinitions(partitionKey)
+                    .keySchema(keySchemaElement)
+                    .provisionedThroughput(provisionedThroughput)
+                    .build();
+
+            CreateTableResponse createTableResponse = dynamoDbClient.createTable(createTableRequest);
+            System.out.println("Created Table: "+ createTableResponse.tableDescription().tableName());
+        }catch(DynamoDbException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }
+        finally {
+            dynamoDbClient.close();
+        }
+    }
+}
