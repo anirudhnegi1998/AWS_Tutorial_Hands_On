ecf01095e4ce0851e5889b76e19fcacfc0f53e16
anirudhnegi1998 <114493617+anirudhnegi1998@users.noreply.github.com>
2024-04-29 14:13:55 -0700

    Configured Time to live request for a table in DynamoDB

---
+++ src/main/java/aws_DynamoDB/TimeToLiveRequest.java
@@ -0,0 +1,28 @@
+package aws_DynamoDB;
+
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.services.dynamodb.model.TimeToLiveSpecification;
+import software.amazon.awssdk.services.dynamodb.model.UpdateTimeToLiveRequest;
+
+public class TimeToLiveRequest {
+    public static void main(String[] args) {
+        DynamoDbClient client = DynamoDbClient.builder().build();
+
+        try{
+            UpdateTimeToLiveRequest request = UpdateTimeToLiveRequest.builder()
+                    .tableName("UserTable")
+                    .timeToLiveSpecification(TimeToLiveSpecification.builder()
+                            .attributeName("September 20, 2024")
+                            .enabled(true)
+                            .build())
+                    .build();
+            client.updateTimeToLive(request);
+            System.out.println("TTL configured");
+        }catch (DynamoDbException e){
+            System.err.println(e.awsErrorDetails().errorMessage());
+        }finally {
+            client.close();
+        }
+    }
+}
